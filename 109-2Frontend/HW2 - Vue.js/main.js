91a6e581b6bad0cb03fa7d4eb2276753efc5055f
Your Name <yuchihsu0517@gmail.com>
2021-06-01 22:10:22 +0800

    HW2

---
+++ 109-2Frontend/HW2 - Vue.js/main.js
@@ -0,0 +1,48 @@
+
+var app = new Vue({
+    el:"#app",
+    data:{
+        brand:"Vue Mastery",
+        product: "Socks",
+        selectedVariant:0,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+            variantId: 2234,
+            variantColor: "green",
+            variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            variantQuantity: 10
+            },
+            {
+            variantId:2235,
+            variantColor: "blue",
+            variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            variantQuantity: 0
+            }
+        ],
+        cart: 0,
+    },
+    methods:{
+        addToCart: function () {
+            this.cart ++
+        },
+        removeFromCart: function(){
+            this.cart --
+        },
+        updateProduct: function(index){
+            this.selectedVariant = index;
+            console.log(index)
+        }
+    },
+    computed:{
+        title(){
+            return this.brand + " " + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        }
+    }
+})
--- 109-2Frontend/try/try.js
+++
@@ -1,3 +0,0 @@
-function myFunction(){
-    document.getElementById("oops").innerHTML = "google!";
-}
